4589c80516b505f0af05f508bcf650834eaf416b
KhushiMalik-KM <khushi.softeng@gmail.com>
2026-01-30 12:53:09 +0530

    Update DBConnectionTest.java

--- DBConnectionTest.java
+++ DBConnectionTest.java
@@ -1,5 +1,20 @@
 package dev;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class DBConnectionTest {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.postgresql.jdbc.Driver");
+			System.out.println("Connected");
+			Connection cn= DriverManager.getConnection("jdbc:postgresql://local host: 5432/rideapp","postgres","khushi");
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 
+		
+	}
 }
+
+
