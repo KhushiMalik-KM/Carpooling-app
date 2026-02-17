2d718d348c27888a6dfeeb5d44ac5cdcc82f7eff
KhushiMalik-KM <khushi.softeng@gmail.com>
2026-02-17 11:03:01 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,54 @@
-package dev;
-
 public class Main {
-	public static void main(String[] args) {
-		RideBookingSystem system=new RideBookingSystem();
+    public static void main(String[] args) {
+        RideBookingSystemDB system = new RideBookingSystemDB();
 
-		User u1=new User("k@mail","khushi","pass",01);
-		User u2=new User("t@mail","tulsi","pass1",07);
-		User u3=new User("m@mail","manishka","pass2",67);
-		
-		system.Signup(u1);
-		system.Signup(u2);
-		system.Signup(u3);
-		
-		User loggedInUser = system.Login("k@mail", "pass");
+        // Create users
+        User u1 = new User("k@mail", "khushi", "pass", 1);
+        User u2 = new User("t@mail", "tulsi", "pass1", 7);
+        User u3 = new User("m@mail", "manishka", "pass2", 67);
+
+        // Sign up users
+        system.Signup(u1);
+        system.Signup(u2);
+        system.Signup(u3);
+
+        // Login
+        User loggedInUser = system.Login("k@mail", "pass");
 
         if (loggedInUser != null) {
-        	Ride ride1=new Ride(1, "Delhi", "Greater Noida", 3, 250, u1);
+            // Create rides
             system.createRide(1, "Delhi", "Greater Noida", 3, 250, u1);
             system.createRide(2, "Jaipur", "Ajmer", 4, 420, u2);
-            Ride ride2=new Ride(2, "Jaipur", "Ajmer", 4, 420, u2);
+
+            // Show all rides
+            System.out.println("\n=== All Available Rides ===");
             System.out.println(system.showAllRides());
 
-            system.bookRide(101, ride1, loggedInUser, 2);
+            // Book a ride (use ride_id instead of Ride object)
+            system.bookRide(101, 1, loggedInUser, 2);
 
-            System.out.println("Rides created by Khushi: " + system.viewCreatedRides(u1));
-            System.out.println("Rides booked by Aman: " + system.viewBookedRides(loggedInUser));
+            // View created rides
+            System.out.println("\n=== Rides created by Khushi ===");
+            System.out.println(system.viewCreatedRides(u1));
 
+            // View booked rides
+            System.out.println("\n=== Rides booked by Khushi ===");
+            System.out.println(system.viewBookedRides(loggedInUser));
+
+            // Update booking
+            System.out.println("\n=== Updating booking ===");
+            system.updateBooking(101, 3);
+
+            // Delete booking
+            System.out.println("\n=== Deleting booking ===");
+            system.deleteBooking(101);
+        }
+    }
+}
             system.updateBooking(101, 3);
             system.deleteBooking(101);
         }
 	}
 
 }
+
