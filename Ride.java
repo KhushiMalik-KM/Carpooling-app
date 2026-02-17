be731cd599a562b56cc0d7065b0dd515f6fa8579
KhushiMalik-KM <khushi.softeng@gmail.com>
2026-02-17 11:03:35 +0530

    Update Ride.java

--- Ride.java
+++ Ride.java
@@ -1,28 +1,25 @@
-package dev;
-
 public class Ride {
-	int ride_id;
-	String source;
-	String destination;
-	int seats;
-	double fare;
-	int available_seats;
-	int total_seats;
-	User user;
-	public Ride(int ride_id, String source, String destination, int seats, double fare, User user) {
-		this.ride_id = ride_id;
-		this.source = source;
-		this.destination = destination;
-		this.seats = seats;
-		this.fare = fare;
-		this.available_seats = seats;
-		this.total_seats = seats;
-	}
-	@Override
-	public String toString() {
-		return "Ride [ride_id=" + ride_id + ", source=" + source + ", destination=" + destination + ", seats=" + seats + ", fare="
-				+ fare + ", available_seats=" + available_seats + ", total_seats=" + total_seats + ", user=" + user + "]";
-	}
-	
-	
+    int ride_id;
+    String source;
+    String destination;
+    int seats;
+    double fare;
+    int available_seats;
+    int total_seats;
+    User user;
+    public Ride(int ride_id, String source, String destination, int seats, double fare, User user) {
+        this.ride_id = ride_id;
+        this.source = source;
+        this.destination = destination;
+        this.seats = seats;
+        this.fare = fare;
+        this.available_seats = seats;
+        this.total_seats = seats;
+        this.user=user;
+    }
+    @Override
+    public String toString() {
+        return "Ride [ride_id=" + ride_id + ", source=" + source + ", destination=" + destination + ", seats=" + seats + ", fare="
+                + fare + ", available_seats=" + available_seats + ", total_seats=" + total_seats + ", user=" + user + "]";
+    }
 }
