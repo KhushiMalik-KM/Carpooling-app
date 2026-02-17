dfcaa8eef888bd802d34fe82f5303c198253b152
KhushiMalik-KM <khushi.softeng@gmail.com>
2026-02-17 10:58:09 +0530

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -1,23 +1,23 @@
-package dev;
 public class Booking {
-	int booking_id;
-	Ride ride;
-	User user;
-	int seats_booked;
-	double total_fare;
-	
-	public Booking(int booking_id, Ride ride, User user, int seats_booked) {
-		super();
-		this.booking_id = booking_id;
-		this.ride = ride;
-		this.user = user;
-		this.seats_booked = seats_booked;
-		this.total_fare = seats_booked*ride.fare;
-	};
-	@Override
-	public String toString() {
-		return "Booking [booking_id=" + booking_id + ", ride=" + ride + ", user=" + user + ", seats_booked="
-				+ seats_booked + ", total_fare=" + total_fare + "]";
-	}
+    int booking_id;
+    Ride ride;
+    User user;
+    int seats_booked;
+    double total_fare;
+
+    public Booking(int booking_id, Ride ride, User user, int seats_booked) {
+        super();
+        this.booking_id = booking_id;
+        this.ride = ride;
+        this.user = user;
+        this.seats_booked = seats_booked;
+        this.total_fare = seats_booked*ride.fare;
+    };
+    @Override
+    public String toString() {
+        return "Booking [booking_id=" + booking_id + ", ride=" + ride + ", user=" + user + ", seats_booked="
+                + seats_booked + ", total_fare=" + total_fare + "]";
+    }
 }
+
 
