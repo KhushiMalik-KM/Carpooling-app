82e2681c22342930ceb4516fdd7aab14cea030ab
KhushiMalik-KM <khushi.softeng@gmail.com>
2026-02-17 11:08:02 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,20 +1,18 @@
-package dev;
-
 public class User {
-	String email;
-	String name;
-	String password;
-	int user_id;
-	public User(String email, String name, String password, int user_id) {
-		super();
-		this.email = email;
-		this.name = name;
-		this.password = password;
-		this.user_id = user_id;
-	}
-	@Override
-	public String toString() {
-		return "User [email=" + email + ", name=" + name + ", password=" + password + ", user_id=" + user_id + "]";
-	}
-	
+    String email;
+    String name;
+    String password;
+    int user_id;
+    public User(String email, String name, String password, int user_id) {
+        super();
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.user_id = user_id;
+    }
+    @Override
+    public String toString() {
+        return "User [email=" + email + ", name=" + name + ", password=" + password + ", user_id=" + user_id + "]";
+    }
+
 }
